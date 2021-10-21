d0c28b758bacac519ada89e189a24081ebe23c33
Dmitry Podelnik <45734486+DmitryPodelnik@users.noreply.github.com>
2021-10-21 10:54:57 +0300

    updated home

--- Gallery/www/js/script.js
+++ Gallery/www/js/script.js
@@ -1,1 +1,22 @@
-console.log("Script works");
+document.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const descr = form.querySelector('input[name=description]');
+    if (!descr) {
+        throw "Data transfer error: input[name=description] not found.";
+    }
+    const place = form.querySelector('input[name=place]');
+    if (!place) {
+        throw "Data transfer error: input[name=place] not found.";
+    }
+    // TODO: data validation 
+
+    const formData = new FormData();
+    formData.append("input[name=description]", descr.value);
+    formData.append("input[name=place]", place.value);
+
+    fetch("/", {
+        method: "GET", 
+        data: formData
+    }).then(r => r.text()).then(console.log);
+});
