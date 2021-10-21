9c4686f5d6d4ef24544855f0ce47e60a0ea57a24
Dmitry Podelnik <45734486+DmitryPodelnik@users.noreply.github.com>
2021-10-22 00:10:03 +0300

    updated home

--- Gallery/index.js
+++ Gallery/index.js
@@ -1,138 +1,139 @@
 // Сервер на JS
-const HTTP_PORT    = 80 ;
-const WWW_ROOT     = "www" ;
-const FILE_404     = WWW_ROOT + "/404.html" ;
-const DEFAULT_MIME = "application/octet-stream" ;
+const HTTP_PORT = 80;
+const WWW_ROOT = "www";
+const FILE_404 = WWW_ROOT + "/404.html";
+const INDEX_HTML = WWW_ROOT + "/index.html";
+const DEFAULT_MIME = "application/octet-stream";
 
 // Подключение модуля
-const http = require( "http" ) ;
-const fs   = require( "fs" ) ;   // file system
+const http = require("http");
+const fs = require("fs");   // file system
 
 // Серверная функция
-function serverFunction( request, response ) {
+function serverFunction(request, response) {
     // определение данных из тела запроса (POST-данных)
     /* Если запрос большой, то тело может передаваться частями (chunk-ами).
        Для работы с телом, его необходимо сначала получить (собрать), затем обрабатывать.
        Приход chunk-а сопровождается событием "data", конец пакета - событие "end" */
     requestBody = [];  // массив chunk-ов
     request.on("data", chunk => requestBody.push(chunk))
-           .on("end", () => {  // конец получения пакета (запроса)
-               request.params = { 
-                   body: Buffer.concat(requestBody).toString() 
-                };
-               analyze(request, response);
-           });
+        .on("end", () => {  // конец получения пакета (запроса)
+            request.params = {
+                body: Buffer.concat(requestBody).toString()
+            };
+            analyze(request, response);
+        });
 }
 
 function analyze(request, response) {
     // логируем запрос - это must have для всех серверов
-    console.log( request.method + " " + request.url );
+    console.log(request.method + " " + request.url);
     // Декодируем запрос: "+" -> пробел, затем decodeURI
     var decodedUrl = request.url.replace(/\+/g, ' ');
-    decodedUrl = decodeURIComponent( decodedUrl);
+    decodedUrl = decodeURIComponent(decodedUrl);
     // разделяем запрос по "?" - отделяем параметры
-    const requestParts = decodedUrl.split( "?" );
+    const requestParts = decodedUrl.split("?");
     // первая часть (до ?) - сам запрос
-    const requestUrl = requestParts[ 0 ] ;
+    const requestUrl = requestParts[0];
     // вторая часть - параметры по схеме key1=val1 & key2=val2
-    var params = {} ;
-    if( requestParts.length > 1         // есть вторая часть
-     && requestParts[1].length > 0 ) {  // и она не пустая
-        for( let keyval of requestParts[1].split( "&" ) ) {
-            let pair = keyval.split( "=" ) ;
-            params[ pair[0] ] = 
+    var params = {};
+    if (requestParts.length > 1         // есть вторая часть
+        && requestParts[1].length > 0) {  // и она не пустая
+        for (let keyval of requestParts[1].split("&")) {
+            let pair = keyval.split("=");
+            params[pair[0]] =
                 typeof pair[1] == 'undefined'
                     ? null
-                    : pair[1] ;
+                    : pair[1];
         }
     }
-    console.log( params ) ;
+    console.log(params);
 
     // проверить запрос на спецсимволы (../)
-    const restrictedParts = [ "../", ";" ] ;
-    for( let part of restrictedParts ) {
-        if( requestUrl.indexOf( part ) !== -1 ) {
+    const restrictedParts = ["../", ";"];
+    for (let part of restrictedParts) {
+        if (requestUrl.indexOf(part) !== -1) {
             // TODO: создать страницу "Опасный запрос"
-            response.statusCode = 418 ;
-            response.setHeader( 'Content-Type', 'text/plain' ) ;
-            response.end( "I'm a teapot" ) ;
-            return ;
+            response.statusCode = 418;
+            response.setHeader('Content-Type', 'text/plain');
+            response.end("I'm a teapot");
+            return;
         }
     }
 
     // проверяем, является ли запрос файлом
-    const path = WWW_ROOT + requestUrl ;
-    if( fs.existsSync( path )   // да, такой объект существует
-     && fs.lstatSync( path ).isFile() ) {  // и это файл        
-        sendFile( path, response ) ;
-        return ;
+    const path = WWW_ROOT + requestUrl;
+    if (fs.existsSync(path)   // да, такой объект существует
+        && fs.lstatSync(path).isFile()) {  // и это файл        
+        sendFile(path, response);
+        return;
     }
     // нет, это не файл. Маршрутизируем
-    const url = requestUrl.substring(1) ;
-    if( url == '' ) {
+    const url = requestUrl.substring(1);
+    if (url == '') {
         // запрос / - передаем индексный файл
-        sendFile( "www/index.html", response ) ;
+        sendFile(INDEX_HTML, response);
     }
-    else if( url.indexOf( "api/" ) == 0 ) {  // запрос начинается с api/
+    else if (url.indexOf("api/") == 0) {  // запрос начинается с api/
         request.params.query = params;
-        processApi( request, response ) ;
-        return ;
+        processApi(request, response);
+        return;
     }
     else {
         // необработанный запрос - "не найдено" (404.html)
-        sendFile( FILE_404, response, 404 ) ;  
+        sendFile(FILE_404, response, 404);
     }
 }
 
 // Создание сервера (объект)
-const server = http.createServer( serverFunction ) ;
+const server = http.createServer(serverFunction);
 
 // Запуск сервера - начало прослушивания порта
 server.listen(  // регистрируемся в ОС на получение 
-                // пакетов, адрессованных на наш порт 
+    // пакетов, адрессованных на наш порт 
     HTTP_PORT,  // номер порта
     () => {  // callback, после-обработчик, вызывается
-             // после того, как "включится слушание"
-        console.log( "Listen start, port " + HTTP_PORT ) ; 
-    } 
-) ;
+        // после того, как "включится слушание"
+        console.log("Listen start, port " + HTTP_PORT);
+    }
+);
 
 // задание
-async function sendFile2( path, response, statusCode ) {
+async function sendFile2(path, response, statusCode) {
     fs.readFile(
         path,
-        ( err, data ) => {
-            if( err ) {
-                console.error( err ) ;
+        (err, data) => {
+            if (err) {
+                console.error(err);
                 return;
             }
-            if( typeof statusCode == 'undefined' )
-                statusCode = 200 ;
-            response.statusCode = statusCode ;
-            response.setHeader( 'Content-Type', 'text/html; charset=utf-8' ) ;
-            response.end( data ) ;
-        } );
+            if (typeof statusCode == 'undefined')
+                statusCode = 200;
+            response.statusCode = statusCode;
+            response.setHeader('Content-Type', 'text/html; charset=utf-8');
+            response.end(data);
+        });
 }
 
 // Stream - piping: stream copy from readable stream to writable
-async function sendFile( path, response, statusCode=200 ) {
-    var readStream = false ;
-    if( fs.existsSync( path ) ) {
-        readStream = fs.createReadStream( path ) ;
+async function sendFile(path, response, statusCode = 200) {
+    var readStream = false;
+    if (fs.existsSync(path)) {
+        readStream = fs.createReadStream(path);
         //if( typeof statusCode == 'undefined' ) statusCode = 200 ;        
-    } else if( fs.existsSync( FILE_404 ) ) {
-        readStream = fs.createReadStream( FILE_404 ) ;
-        statusCode = 404 ;
-    }    
-    
-    if( readStream ) {
-        response.statusCode = statusCode ;
-        response.setHeader( 'Content-Type', getMimeType( path ) ) ;
-        readStream.pipe( response ) ;
+    } else if (fs.existsSync(FILE_404)) {
+        readStream = fs.createReadStream(FILE_404);
+        statusCode = 404;
+    }
+
+    if (readStream) {
+        response.statusCode = statusCode;
+        response.setHeader('Content-Type', getMimeType(path));
+        readStream.pipe(response);
     } else {
-        response.statusCode = 418 ;
-        response.setHeader( 'Content-Type', 'text/plain' ) ;
-        response.end( "I'm a teapot" ) ;
+        response.statusCode = 418;
+        response.setHeader('Content-Type', 'text/plain');
+        response.end("I'm a teapot");
     }
 
     // задание: проверить наличие файла перед отправкой:
@@ -142,44 +143,44 @@ async function sendFile( path, response, statusCode=200 ) {
 }
 
 // returns Content-Type header value by parsing file name (path)
-function getMimeType( path ) {
+function getMimeType(path) {
     // file extension
-    if( ! path ) {
-        return false ;
+    if (!path) {
+        return false;
     }
-    const dotPosition = path.lastIndexOf( '.' ) ;
-    if( dotPosition == -1 ) {  // no extension
-        return DEFAULT_MIME ;
+    const dotPosition = path.lastIndexOf('.');
+    if (dotPosition == -1) {  // no extension
+        return DEFAULT_MIME;
     }
-    const extension = path.substring( dotPosition + 1 ) ;
-    switch( extension ) {
-        case 'html' :
-        case 'css'  :
-            return 'text/' + extension ;
-        case 'jpeg' :
-        case 'jpg'  :
-            return 'image/jpeg' ;
-        case 'bmp'  :
-        case 'gif'  :
-        case 'png'  :
-            return 'image/' + extension ;
-        case 'json' :
-        case 'pdf'  :
-        case 'rtf'  :
-            return 'application/' + extension ;
-        default :
-            return DEFAULT_MIME ;
+    const extension = path.substring(dotPosition + 1);
+    switch (extension) {
+        case 'html':
+        case 'css':
+            return 'text/' + extension;
+        case 'jpeg':
+        case 'jpg':
+            return 'image/jpeg';
+        case 'bmp':
+        case 'gif':
+        case 'png':
+            return 'image/' + extension;
+        case 'json':
+        case 'pdf':
+        case 'rtf':
+            return 'application/' + extension;
+        default:
+            return DEFAULT_MIME;
     }
 }
 
 // Обратка запросов   api/*
-async function processApi( request, response ) {
-    var res = {} ;
+async function processApi(request, response) {
+    var res = {};
 
-    res.status = "Works" ;
+    res.status = "Works";
     // упражнение: включить в ответ все принятые параметры запроса
     res.params = request.params;
-    
-    response.setHeader( 'Content-Type', 'application/json' ) ;
-    response.end( JSON.stringify( res ) ) ;
+
+    response.setHeader('Content-Type', 'application/json');
+    response.end(JSON.stringify(res));
 }
